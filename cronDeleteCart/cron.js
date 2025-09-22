198d86d9c0ebe074b58d45daecfa5ab4a7c9ceee
DmitrySobol1 <saibleonetwo@gmail.com>
2025-09-22 15:46:03 +0300

    22sep  10

--- cronDeleteCart/cron.js
+++ cronDeleteCart/cron.js
@@ -11,7 +11,6 @@ dotenv.config({ path: '/root/wassermatrix/wassermatrixBack/.env' });
 
 
 cron.schedule(
-//   '0 10 * * *',  
   '0/2 * * * *',
   async () => {
     console.log('🚀 Запуск CRON1 - delete carts', new Date().toISOString());
